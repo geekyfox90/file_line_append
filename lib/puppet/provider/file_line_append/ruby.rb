b86d48dafa37bdae9c71a9207aca16725f22f923
Yasser B <boukhersy@gmail.com>
2017-10-29 15:45:39 +0100

    update

--- lib/puppet/provider/file_line_append/ruby.rb
+++ lib/puppet/provider/file_line_append/ruby.rb
@@ -1,57 +1,57 @@
-Puppet::Type.type(:file_line_append).provide(:ruby) do 
-       
-def exists?
-   line = getline()
-   exists(line) 
-end
-    
-def create
-   line = getline()   
-   newline = add_string(line)
-   file = File.read(resource[:path])
-   new_contents = file.gsub(line, newline)
-   File.open(resource[:path], "w") {|file| file.puts new_contents }
-end
- 
-def destroy
-   line = getline()   
+Puppet::Type.type(:file_line_append).provide(:ruby) do
+
+  def exists?
+    line = getline()
+    exists(line)
+  end
+
+  def create
+    line = getline()
+    newline = add_string(line)
+    file = File.read(resource[:path])
+    new_contents = file.gsub(line, newline)
+    File.open(resource[:path], "w") {|file| file.puts new_contents}
+  end
+
+  def destroy
+    line = getline()
     newline = delete_string(line)
-   file = File.read(resource[:path])
-   new_contents = file.gsub(line, newline)
-   File.open(resource[:path], "w") {|file| file.puts new_contents }  
-end    
-    
-def getline()
-   file = File.readlines(resource[:path])
-   matches = file.select { |line| line.match(resource[:line]) }
-   line = matches[0]
-   return line
-end    
+    file = File.read(resource[:path])
+    new_contents = file.gsub(line, newline)
+    File.open(resource[:path], "w") {|file| file.puts new_contents}
+  end
 
-def exists(line)
+  def getline()
+    file = File.readlines(resource[:path])
+    matches = file.select {|line| line.match(resource[:line])}
+    line = matches[0]
+    return line
+  end
+
+  def exists(line)
     if line.include? resource[:string]
-        return true
+      return true
     else
-        return false
+      return false
     end
-end
+  end
 
-def add_string(line)
+  def add_string(line)
     if (resource[:match])
-        line = line.gsub(resource[:match],resource[:string])    
+      line = line.gsub(resource[:match], resource[:string])
     else
-     if line.include? "\n"
-        line = line.gsub(/\n/,"")
-        resource[:string] << "\n"    
-     end  
-     line << resource[:string]
+      if line.include? "\n"
+        line = line.gsub(/\n/, "")
+        resource[:string] << "\n"
+      end
+      line << resource[:string]
     end
     return line
-end   
+  end
 
-def delete_string(line) 
-    line = line.gsub(resource[:string],"")
+  def delete_string(line)
+    line = line.gsub(resource[:string], "")
     return line
-end
-       
+  end
+
 end
--- lib/puppet/type/file_line_append.rb
+++ lib/puppet/type/file_line_append.rb
@@ -1,31 +1,43 @@
 Puppet::Type.newtype(:file_line_append) do
-    
-       ensurable do
-          defaultvalues
-          defaultto :present
-       end
-    
-       newparam(:name, :namevar => true) do
-           desc 'An arbitrary name used as the identity of the resource.'
-       end
-          
-       newparam(:string) do
-           desc "The string to be appended to the line or used to replace matches found by the match attribute."
-       end
-       newparam(:path) do
-          desc 'The file Puppet will ensure contains the line specified by the line parameter.'
-          validate do |value|
-           unless Puppet::Util.absolute_path?(value)
-               raise Puppet::Error, "File paths must be fully qualified, not '#{value}'"
-           end
-        end
-       end
-       newparam(:match) do
-           desc 'an option string to be used to repalce the string found, if not specified, the new string will be appended at the end of the line.'
-       end
 
-       newparam(:line) do
-           desc 'The line where the string will be appended.'
-       end
-    
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'An arbitrary name used as the identity of the resource.'
+  end
+
+  newparam(:string) do
+    desc "The string to be appended to the line or used to replace matches found by the match attribute."
+  end
+  newparam(:path) do
+    desc 'The file Puppet will ensure contains the line specified by the line parameter.'
+    validate do |value|
+      unless Puppet::Util.absolute_path?(value)
+        raise Puppet::Error, "File paths must be fully qualified, not '#{value}'"
+      end
+    end
+  end
+  newparam(:match) do
+    desc 'an option string to be used to repalce the string found, if not specified, the new string will be appended at the end of the line.'
+  end
+
+  newparam(:line) do
+    desc 'The line where the string will be appended.'
+  end
+
+  validate do
+    unless self[:path]
+      raise(Puppet::Error, "path is a required attribute")
+    end
+    unless self[:string]
+      raise(Puppet::Error, "string is a required attribute")
+    end
+    unless self[:line]
+      raise(Puppet::Error, "line is a required attribute")
+    end  
+  end
+
 end
