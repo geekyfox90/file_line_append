7d20580b7cb2556291bb4a380f88e4db59de1c1a
Yasser B <boukhersy@gmail.com>
2017-10-29 12:00:39 +0100

    test

--- lib/puppet/type/file_line_append.rb
+++ lib/puppet/type/file_line_append.rb
@@ -1,11 +1,26 @@
 Puppet::Type.newtype(:file_line_append) do
     
-      ensurable
+     ensurable do
+        defaultvalues
+        defaultto :present
+     end
           
-      newparam(:name) do
-          desc "The name of the variable."
-      end
-      newparam(:value) do
-          desc "The value of the env variable."
-      end
+       newparam(:string) do
+           desc "The string to be appended to the line or used to replace matches found by the match attribute."
+       end
+       newparam(:path) do
+          desc 'The file Puppet will ensure contains the line specified by the line parameter.'
+          validate do |value|
+           unless Puppet::Util.absolute_path?(value)
+               raise Puppet::Error, "File paths must be fully qualified, not '#{value}'"
+           end
+        end
+       end
+       newparam(:match) do
+           desc 'an option string to be used to repalce the string found, if not specified, the new string will be appended at the end of the line.'
+       end
+
+       newparam(:line) do
+           desc 'The line where the string will be appended.'
+       end
     end
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class file_line_append::init {
 
   file_line_append {'test file line append':
+       ensure => present,
+       file => "",
        name => "test",
        value => "test"
   }
